4af0549b2848fbbd4432d1549b0546843f45369d
Celtic Minstrel <celtic.minstrel.ca@some.place>
2016-06-10 20:34:36 -0400

    Add new translation library (from cbeck88/spirit-po@0fea1743262f8f5095b0e5db4bb5505e5c4656ee )

---
+++ src/spirit_po/po_message_adapted.hpp
@@ -0,0 +1,23 @@
+//  (C) Copyright 2015 - 2016 Christopher Beck
+
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <boost/fusion/adapted/struct/adapt_struct.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/std_pair.hpp>
+
+#include <boost/optional/optional.hpp>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <spirit_po/po_message.hpp>
+
+BOOST_FUSION_ADAPT_STRUCT(
+  spirit_po::po_message,
+  (boost::optional<std::string>, context)
+  (std::string, id)
+  (spirit_po::plural_and_strings_type, plural_and_strings))
